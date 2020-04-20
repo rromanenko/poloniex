d6f50004a999a6fb2c7bdf6c918343e657fc1ecb
Roman Romanenko <twaresare@gmail.com>
2020-04-20 22:46:41 +0300

    Removing duplicate files, refactored lending bot

--- lending_bot.py
+++ lending_bot.py
@@ -8,16 +8,11 @@ Created on Sat Jul  7 23:18:58 2018
 
 from poloniex_api import Poloniex
 from polo_config import *
+import smtplib      #import for SMTP
+import datetime     #import for comparing open loans time with now()
 
-#import for SMTP
-import smtplib
-
-#import for comparing open loans time with now()
-import datetime
-
-
-def email_send(subj,message):
-    # send an email with all details
+# send an email with all details
+def email_send(subj, message):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     # server.ehlo()
     server.starttls()
@@ -36,45 +31,45 @@ def email_send(subj,message):
     server.sendmail(from_email, to_email, msg)
     server.quit()
 
+# api key/secret pair for lending in config file
+my_polo = Poloniex( API_KEY = key_lend, API_SECRET = secret_lend)
 
-# take api key/secret pair for lending
-my_polo = Poloniex(
-  API_KEY = key_lend,
-  API_SECRET = secret_lend)
+def cancel_loans():
+    # getting my open loan offers
+    # e.g. my_open_offers = {'BTC': [{'id': 1053294329, 'rate': '0.00020000', 'amount': '0.20513671', 'duration': 2, 'autoRenew': 0, 'date': '2019-01-06 08:42:47'}]}
+    my_open_offers = my_polo.returnOpenLoanOffers()
 
-# getting my open loan offers
-my_open_offers = my_polo.returnOpenLoanOffers()
+    # if server returns something and there are offers in the response ...
+    if my_open_offers and 'BTC' in my_open_offers:
 
-# if server returns something and there are offers in the response ...
-if my_open_offers and 'BTC' in my_open_offers:
+        # ... cycle through all offers and compare offer datetime with now
+        # if offer was placed more than 30 min ago, cancel it
+        for i in range(len(my_open_offers['BTC'])):
 
-    # ... cycle through all offers and compare offer datetime with now
-    # if offer was placed more than 30 min ago, cancel it
-    for i in range(len(my_open_offers['BTC'])):
+            now = datetime.datetime.now()
+            date = my_open_offers['BTC'][i]['date']
+            dt = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
 
-        now = datetime.datetime.now()
-        date = my_open_offers['BTC'][i]['date']
-        dt = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+            if ((now - dt).total_seconds() / 60 - 180) > 10:
+                try:
+                    cancel = my_polo.cancelLoanOffer(orderNumber=my_open_offers['BTC'][i]['id'])
+                except:
+                    email_send("Polo: error", "Error cancelling order ID:" + str(my_open_offers['BTC'][i]['id']))
+    elif not my_open_offers:
+        pass
+    else:
+        email_send("Polo: error", "Error response for my open offers")
 
-        if ((now - dt).total_seconds() / 60 - 180) > 10:
-            try:
-                cancel = my_polo.cancelLoanOffer(orderNumber=my_open_offers['BTC'][i]['id'])
-            except:
-                email_send("Polo: error", "Error cancelling order ID:" + str(my_open_offers['BTC'][i]['id']))
-elif not my_open_offers:
-    pass
-else:
-    email_send("Polo: error","Error response for my open offers")
+#cancel loans that haven't been claimed for more than 30 min
+cancel_loans()
 
-balance = my_polo.returnAvailableAccountBalances("lending")
+# getting balance from poloniex, e.g. {'exchange': {'BTT': '500.00000000'}, 'lending': {'BTC': '0.00016670'}}
+balance = my_polo.returnAvailableAccountBalances()
 
-if balance:
+# if balance not empty and available balance is larger than minimal lending amount of 0.005 BTC
+if 'lending' in balance and float(balance['lending']['BTC']) > 0.005:
+
     av_balance = float(balance['lending']['BTC'])
-else:
-    av_balance = 0
-
-if av_balance >= 0.01:
-
     loan_offers = my_polo.returnLoanOrders(currency='BTC')
     amount = 0
     for i in range(len(loan_offers['offers'])):
@@ -86,12 +81,12 @@ if av_balance >= 0.01:
 
     pos_info = my_polo.createLoanOffer(
             currency='BTC',
-            amount=av_balance,
+            amount = av_balance,
             duration=2,
             autoRenew=0,
             lendingRate=float(loan_offers['offers'][i]['rate'])
             )
-    
+
     if 'success' not in pos_info:
         pos_info['amount'] = av_balance
         pos_info['rate'] = ("%f" % float(loan_offers['offers'][i]['rate']) )
